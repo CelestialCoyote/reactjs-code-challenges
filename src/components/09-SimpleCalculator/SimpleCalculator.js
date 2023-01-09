f5f09ba2802d94c50ab5b12b56b70d880ac0ab29
Paul Stearns <celestialcoyote@gmail.com>
2023-01-09 15:40:13 -0800

    SimpleCalculator challenge completed.

--- src/components/09-SimpleCalculator/SimpleCalculator.js
+++ src/components/09-SimpleCalculator/SimpleCalculator.js
@@ -1,12 +1,34 @@
+import { useReducer } from 'react';
 import './SimpleCalculator.css';
 
 
-const initialState = {}
+const initialState = {
+    num1: 0,
+    num2: 0,
+    result: 'no result'
+}
 
-function reducer(state, action) { }
+function reducer(state, action) {
+    switch (action.type) {
+        case 'SET_NUM_ONE':
+            return { ...state, num1: action.number };
+        case 'SET_NUM_TWO':
+            return { ...state, num2: action.number };
+        case 'ADD':
+            return { ...state, result: state.num1 + state.num2 };
+        case 'SUBTRACT':
+            return { ...state, result: state.num1 - state.num2 };
+        case 'CLEAR':
+            return { ...state, num1: 0, num2: 0, result: 'no result' };
+        default:
+            return state;
+
+    }
+}
 
 const SimpleCalculator = () => {
-    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     return (
         <div>
@@ -16,6 +38,7 @@ const SimpleCalculator = () => {
                     <button
                         key={number}
                         className='calculator-button'
+                        onClick={() => dispatch({ type: 'SET_NUM_ONE', number })}
                     >
                         {number}
                     </button>))}
@@ -26,17 +49,33 @@ const SimpleCalculator = () => {
                     <button
                         key={number}
                         className='calculator-button'
+                        onClick={() => dispatch({ type: 'SET_NUM_TWO', number })}
                     >
                         {number}
                     </button>))}
             </div>
             <div>
                 <h2>Actions</h2>
-                <button className='calculator-button'>+</button>
-                <button className='calculator-button'>-</button>
-                <button className='calculator-button'>c</button>
+                <button
+                    className='calculator-button'
+                    onClick={() => dispatch({ type: 'ADD' })}
+                >
+                    +
+                </button>
+                <button
+                    className='calculator-button'
+                    onClick={() => dispatch({ type: 'SUBTRACT' })}
+                >
+                    -
+                </button>
+                <button
+                    className='calculator-button'
+                    onClick={() => dispatch({ type: 'CLEAR' })}
+                >
+                    c
+                </button>
             </div>
-            <div><h2>Result:</h2></div>
+            <div><h2>Result: {state.result}</h2></div>
         </div>
     );
 };
